eff01a4b2f5a796e362c0b0f3f87a9f1c786369b
phytertek <ryan.phytertek@gmail.com>
2017-10-05 10:07:36 -0400

    add if checkes for hooks and mocks in the schema builder

--- mongo/buildModel.js
+++ mongo/buildModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const GlobalSchema = require('./Schema');
 module.exports = Object.keys(GlobalSchema).reduce((DB, Model) => {
-  const SchemaEntries = GlobalSchema[Model].reduce((r, e) => ({ ...r, ...e.Schema }), {})
+  const SchemaEntries = GlobalSchema[Model].reduce((r, e) => ({ ...r, ...e.Schema }), {});
   const ModelSchema = new mongoose.Schema(SchemaEntries);
   GlobalSchema[Model].forEach(e => {
-    e.Hooks(ModelSchema)
-    e.Methods(ModelSchema)
+    if (e.Hooks) e.Hooks(ModelSchema);
+    if (e.Methods) e.Methods(ModelSchema);
   });
   DB[Model] = mongoose.model(Model, ModelSchema)
   return DB
